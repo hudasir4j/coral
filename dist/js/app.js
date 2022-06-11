531e688acc6c20d757aaf1c5b869cf80dbd7b4f7
hudasir4j <hudasir4j@gmail.com>
2022-06-11 11:07:53 -0700

    home page
    todo for homepage:
    - fix responsive navbar
    - less harsh color choice for title?
    
    -- ONTO VOLUNTEER PAGE!!!!

---
+++ dist/js/app.js
@@ -0,0 +1,9 @@
+const checkbox = document.getElementById('checkbox');
+const body = document.body;
+checkbox.addEventListener('change',()=>{
+    if (body.classList.contains('dark')){
+        body.classList.toggle('light');
+    }else{
+        body.classList.toggle('dark');
+    }
+});
